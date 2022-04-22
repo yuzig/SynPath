225de7407cb87577322746c20c4c28cc74304c50
carolgyz <carol_gyz@berkeley.edu>
2022-04-22 09:43:03 -0700

    update ExtractCoreMet.java and ExtractRxns.java

---
+++ src/org/Retrosynthesis/ExtractCoreMet.java
@@ -0,0 +1,33 @@
+package org.Retrosynthesis;
+
+import org.Retrosynthesis.models.Chems;
+import org.Retrosynthesis.models.Rxns;
+import org.Utils.FileUtils;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class ExtractCoreMet {
+    public void initiate() {
+    }
+    public List<String> run(String metpath) throws Exception{
+        List<String> natives = new ArrayList<>();
+        String data = FileUtils.readFile(metpath);
+        String[] lines = data.trim().split("\\r|\\r?\\n");
+        for (int i = 2; i < lines.length; i++) {
+            String aCompound = lines[i];
+            String[] tabs = aCompound.split(",");
+            String chem = tabs[3];
+            chem = chem.replaceAll("META:","");
+            chem = chem.replaceAll("'","");
+            chem = chem.replaceAll("[\\[\\](){}]","");
+            chem = chem.replaceAll("^\"|\"$", "");
+            if (!natives.contains(chem)) {
+                natives.add(chem);
+            }
+        }
+        return natives;
+    }
+
+}
---
+++ src/org/Retrosynthesis/ExtractRxns.java
@@ -0,0 +1,68 @@
+package org.Retrosynthesis;
+
+import org.Retrosynthesis.models.Chems;
+import org.Retrosynthesis.models.Rxns;
+import org.Utils.FileUtils;
+
+import java.util.*;
+
+public class ExtractRxns {
+    public void initiate(){
+
+    }
+    public List<Rxns> run(String filepath, HashMap<String, Chems> allchems) throws Exception {
+        List<Rxns> allRxn = new ArrayList<>();
+        String rxndata = FileUtils.readFile(filepath);
+        String[] lines = rxndata.trim().split("//");
+        rxndata = null;
+        String uniqueID = null;
+        String ECnum = null;
+        Set<Chems> substrates = new HashSet<>();
+        Set<Chems> products = new HashSet<>();
+
+        for (int i = 2; i < lines.length; i++) {
+            String aRxn = lines[i];
+            String[] aline = aRxn.trim().split("\\r|\\r?\\n");
+            String[] tabs;
+
+            for (String str : aline) {
+                if (str.startsWith("UNIQUE-ID")) {
+                    tabs = str.split(" - ");
+                    uniqueID = tabs[1];
+                    continue;
+                }
+                if (str.startsWith("EC-NUMBER")) {
+                    tabs = str.split(" - ");
+                    ECnum = tabs[1];
+                    continue;
+                }
+                if (str.startsWith("LEFT")) {
+                    tabs = str.split(" - ");
+                    Chems substrate = allchems.get(tabs[1]);
+                    if (substrate == null){
+                        substrate = new Chems(tabs[1],null, null);
+                    }
+                    substrates.add(substrate);
+                    continue;
+                }
+                if (str.startsWith("RIGHT")) {
+                    tabs = str.split(" - ");
+                    Chems product = allchems.get(tabs[1]);
+                    if (product == null){
+                        product = new Chems(tabs[1],null, null);
+                    }
+                    products.add(product);
+                    continue;
+                }
+            }
+            Rxns rxn = new Rxns(ECnum, substrates, products, uniqueID);
+            allRxn.add(rxn);
+            substrates = new HashSet<>();
+            products = new HashSet<>();
+
+        }
+        return allRxn;
+    }
+
+}
+
